f4df3b0af046c0bef0d8eae5ece91d80a0d464de
Jacob Nangle <Jacob.Nangle@gmail.com>
2013-05-08 12:02:17 -0400

    Finished basic populate script

--- populate.sql
+++ populate.sql
@@ -4,10 +4,12 @@ INSERT INTO Professor (`name`) VALUE ('Professor Test');
 INSERT INTO Professor (`name`) VALUE ('Professor Awesome');
 INSERT INTO Professor (`name`) VALUE ('Professor Professorson');
 
+
 INSERT INTO Class (`letterCode`, `number`, `name`) VALUES ('CS', '461', 'Database Systems');
 INSERT INTO Class (`letterCode`, `number`, `name`) VALUES ('Hist', '354', 'Conspiracy Theories in US History');
-INSERT INTO Class (`letterCode`, `number`, `name`) VALUES ('Test', '563', 'Testing of tests');
+INSERT INTO Class (`letterCode`, `number`, `name`) VALUES ('Test', '563', 'Testing of Tests');
 INSERT INTO Class (`letterCode`, `number`, `name`) VALUES ('CS', '324', 'The Class in the Language You Don\'t Know');
+
 
 INSERT INTO Professor_has_Class (`Class_idClass`, `Professor_idProfessor`) 
 	SELECT Class.idClass, Professor.idProfessor
@@ -19,7 +21,7 @@ INSERT INTO Professor_has_Class (`Class_idClass`, `Professor_idProfessor`)
 	SELECT Class.idClass, Professor.idProfessor
 	FROM Professor JOIN Class
 	WHERE 	Professor.name = 'Professor Awesome' AND
-			Class.name = 'Testing of tests';
+			Class.name = 'Testing of Tests';
 
 INSERT INTO Professor_has_Class (`Class_idClass`, `Professor_idProfessor`) 
 	SELECT Class.idClass, Professor.idProfessor
@@ -40,16 +42,75 @@ INSERT INTO Professor_has_Class (`Class_idClass`, `Professor_idProfessor`)
 	WHERE 	Professor.name = 'Professor Awesome' AND
 			Class.name = 'Database Systems';
 
+
 INSERT INTO TagList (tagName) VALUE ('Useless');
 INSERT INTO TagList (tagName) VALUE ('Sample Test');
 INSERT INTO TagList (tagName) VALUE ('Sample Paper');
-INSERT INTO TagList (tagName) VALUE ('Why or why did I think this was a good idea?');
+INSERT INTO TagList (tagName) VALUE ('Why oh why did I think this was a good idea?');
 
-INSERT INTO Crib (name, filePath) VALUES ('Test Crib Name', 'path/to/test/crib');
-INSERT INTO Crib (name, filePath) VALUES ('Conspiracy Crib', 'path/to/Conspiracy/crib');
-INSERT INTO Crib (name, filePath) VALUES ('Langauge Crib', 'path/to/Language/crib');
-INSERT INTO Crib (name, filePath) VALUES ('Testing Crib', 'path/to/Testing/crib');
-INSERT INTO Crib (name, filePath) VALUES ('Demo Test', 'path/to/Demo Test/crib');
-INSERT INTO Crib (name, filePath) VALUES ('Demo Language', 'path/to/Demo Language/crib');
-INSERT INTO Crib (name, filePath) VALUES ('This is not a crib conspiracy', 'path/to/not a crib conspiracy/crib');
 
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass) 
+	SELECT 'Test Crib Name', 'path/to/test/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Test' AND Class.name = 'Testing of Tests';
+
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass)  
+	SELECT 'Conspiracy Crib', 'path/to/Conspiracy/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Professorson' AND Class.Name = 'Conspiracy Theories in US History';
+
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass)  
+	SELECT 'Langauge Crib', 'path/to/Language/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Awesome' AND Class.name = 'The Class in the Language You Don\'t Know';;
+
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass) 
+	SELECT 'Testing Crib', 'path/to/Testing/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Test' AND Class.name = 'Testing of Tests';
+
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass) 
+	SELECT 'Demo Test', 'path/to/Demo Test/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Test' AND Class.name = 'Testing of Tests';
+
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass) 
+	SELECT 'Demo Language', 'path/to/Demo Language/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Awesome' AND Class.name = 'The Class in the Language You Don\'t Know';
+
+INSERT INTO Crib (name, filePath, Professor_idProfessor, Class_idClass) 
+	SELECT 'This is not a crib conspiracy', 'path/to/not a crib conspiracy/crib', Professor.idProfessor, Class.idClass
+	FROM Professor JOIN Class
+	WHERE Professor.name = 'Professor Professorson' AND Class.Name = 'Conspiracy Theories in US History';
+
+
+INSERT INTO Crib_has_Tag (Crib_idCrib, TagList_idTagList)
+	SELECT Crib.idCrib, TagList.idTagList
+	FROM Crib JOIN TagList
+	WHERE 	Crib.name = 'Test Crib Name' AND
+			TagList.tagName = 'Useless';
+
+INSERT INTO Crib_has_Tag (Crib_idCrib, TagList_idTagList)
+	SELECT Crib.idCrib, TagList.idTagList
+	FROM Crib JOIN TagList
+	WHERE 	Crib.name = 'Conspiracy Crib' AND
+			TagList.tagName = 'Sample Test';
+
+INSERT INTO Crib_has_Tag (Crib_idCrib, TagList_idTagList)
+	SELECT Crib.idCrib, TagList.idTagList
+	FROM Crib JOIN TagList
+	WHERE 	Crib.name = 'Demo Language' AND
+			TagList.tagName = 'Sample Paper';
+
+INSERT INTO Crib_has_Tag (Crib_idCrib, TagList_idTagList)
+	SELECT Crib.idCrib, TagList.idTagList
+	FROM Crib JOIN TagList
+	WHERE 	Crib.name = 'Langauge Crib' AND
+			TagList.tagName = 'Sample Test';
+
+INSERT INTO Crib_has_Tag (Crib_idCrib, TagList_idTagList)
+	SELECT Crib.idCrib, TagList.idTagList
+	FROM Crib JOIN TagList
+	WHERE 	Crib.name = 'This is not a crib conspiracy' AND
+			TagList.tagName = 'Why oh why did I think this was a good idea?';
